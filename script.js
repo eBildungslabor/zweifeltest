1fc58e818e987015ffe67278faf24958f1562cd9
Nele Hirsch <nele@ebildungslabor.de>
2020-10-17 20:32:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,6 @@ submitButton.onclick = (e) => {
   }
   
   form.style.display = 'none';
-  result.innerHTML = `Hallo ${name},<br>Zweifel wie z.B. - ${zweifel} - können Ansporn sein, <br>um Neues zu erkunden, Dich auszutauschen und Dir Offenheit zum Lernen zu bewahren<br>.Lass die Zweifel aber nicht so stark werden,<br> dass sie Dich kaputt machen.<p>Eines musst Du unbedingt wissen:<br><strong>Du bist toll und ganz genau richtig, so wie Du bist!</strong></p><p>Und wenn Du das nicht glaubst, dann <a href=https://memegen.link/custom/Zuviel_Zweifel_ist_nicht_gut/${name},_Du_bist_toll!.jpg?alt=https://ebildungslabor.github.io/zweifeltest/meme.jpg>klicke hier</a>`;
+  result.innerHTML = `<h1>Auflösung für ${name}</h1>Zweifel (zum Beispiel: ${zweifel}?) können Ansporn sein, <br>um Neues zu erkunden, Dich auszutauschen und Dir Offenheit zum Lernen zu bewahren.<br>Lass die Zweifel aber nicht so stark werden,<br> dass sie Dich kaputt machen und Du nur noch haderst.<br>Denn eines musst Du unbedingt wissen, ${name}:<br><strong>Du bist toll und ganz genau richtig, so wie Du bist!</strong></p><p>Und wenn Du immer noch an Dir zweifelst, dann <a href=https://memegen.link/custom/Zuviel_Zweifel_ist_nicht_gut/${name},_Du_bist_toll!.jpg?alt=https://ebildungslabor.github.io/zweifeltest/meme.jpg>klicke hier</a>`;
   result.style.display = 'block';
 };
